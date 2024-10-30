a0d6b757af9620296e504966be9fe92d550a263f
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-10-31 01:19:43 +0300

    Create Order_the_num_inputed.java

---
+++ Class_Excercise/Order_the_num_inputed.java
@@ -0,0 +1,44 @@
+// order of 3 numbers
+
+//  Que 1,  sum of the digit
+import java.util.Scanner;
+class Sumofdigits{
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter the 1st number : ");
+        int num1 = console.nextInt();
+        System.out.print("Enter the 2nd number : ");
+        int num2 = console.nextInt();
+        System.out.print("Enter the 3rd number : ");
+        int num3 = console.nextInt();
+        
+        if (num1 > num2 && num2 > num3)
+        {
+            System.out.println("The order of "+ num1 + num2 + num3 + "  is " + num3 +" "+ num2 +" "+ num1 );
+            };
+        if(num1 < num2 && num2 < num3)
+        {
+            System.out.println("The order of "+ num1 + num2 + num3 + "  is " + num1 + num2 + num3 );
+        };
+        if(num1 < num2 && num2 > num3)
+        {
+            System.out.println("The order of "+ num1 + num2 + num3 + "  is " + num1 + num2 + num3 );
+        };
+        if(num1 < num2 && num2 > num3)
+        {
+            System.out.println("The order of "+ num1 + num2 + num3 + "  is " + num1 + num2 + num3 );
+        };
+        if(num1 < num2 && num2 > num3)
+        {
+            System.out.println("The order of "+ num1 + num2 + num3 + "  is " + num1 + num2 + num3 );
+        };
+        
+        else
+        {
+            System.out.println("The order of "+ num1 + num2 + num3 + "  is " + num1 + num2 + num3 );
+        };
+
+        
+        
+}
+}
