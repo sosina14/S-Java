9fb9d7b39203ba33887d956c5ea732a503d4593e
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-11-01 16:45:00 +0300

    Create Q1.largest_number.java

---
+++ Lab Exercise/lab1/Q1.largest_number.java
@@ -0,0 +1,20 @@
+//Que 1,To find Largest of two numebrs
+
+import java.util.Scanner;
+class FristLab{
+  public static void main (String[] args){
+      Scanner console = new Scanner(System.in);
+      System.out.print("Enter the 1st number : ");
+      int num1 = console.nextInt();
+      System.out.print("Enter the 2nd number : ");
+      int num2 = console.nextInt();
+      if (num1 > num2){
+          System.out.print(" "+num1+ " is the Greatest ");
+      }
+      else{
+          System.out.print(" "+num2+ " is the Greatest ");
+      }
+ } 
+}
+
+//Que 2 loop pattern
