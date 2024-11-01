26b3c840c7eda08411e4cb2fbc3762adf5c41423
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-11-01 16:40:55 +0300

    Create Q2.2 number_in_ascending_order.java

---
+++ Lab Exercise/lab1/Q2.2 number_in_ascending_order.java
@@ -0,0 +1,16 @@
+// Que 2.2 print number in ascending order 
+import java.util.*;
+import java.util.Scanner;
+
+public class number_in_ascending_order {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+    System.out.print("Enter the numebr : ");
+        int n = console.nextInt();
+        int i;
+        for (i = 1; i <= n; i++)
+       {
+        System.out.print(i + " "); 
+        }
+             }
+                  }
